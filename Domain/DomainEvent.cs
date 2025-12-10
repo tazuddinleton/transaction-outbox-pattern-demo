ca59074745c300245f2e8371e47d7d8dc4f61705
Taz Uddin <getleton@gmail.com>
2025-12-10 11:20:40 +0600

    Update

--- Domain/DomainEvent.cs
+++ Domain/DomainEvent.cs
@@ -1,6 +1,13 @@
 namespace TransactionOutboxDemo.Domain;
 
-public abstract record DomainEvent
+public interface IDomainEvent
+{
+    Guid EventId { get; }
+    DateTime OccurredOn { get; }
+    string RoutingKey { get; }
+}
+
+public abstract record DomainEvent : IDomainEvent
 {
     public Guid EventId { get; init; } = Guid.NewGuid();
     public DateTime OccurredOn { get; init; } = DateTime.UtcNow;
